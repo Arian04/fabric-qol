7c765d20aa03a2b56510e79016ffb94276f15d6a
Arian Baishya <arian.baishya2004@gmail.com>
2023-12-04 20:53:26 -0600

    Initial Commit
    - Main feature works
    - TODO: convert into a general qol mod

---
+++ src/main/java/me/arianb/bonemealallflowers/mixin/FlowerBlockMixin.java
@@ -0,0 +1,31 @@
+package me.arianb.bonemealallflowers.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(FlowerBlock.class)
+public abstract class FlowerBlockMixin implements Fertilizable {
+
+    @Override
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        FlowerBlock.dropStack(world, pos, new ItemStack((FlowerBlock)(Object)this));
+    }
+}
